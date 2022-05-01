18b3cd8e161941dcf884f33b5f274858daa5c635
IrisIq <2637852337@qq.com>
2022-05-01 20:03:35 +0800

    day 3

--- src/components/page/posGp.vue
+++ src/components/page/posGp.vue
@@ -1,10 +1,85 @@
 <template>
   <div class="pos">
     <el-row>
-      <el-col :span="8"
+      <el-col :span="10"
               class="pos-order"
-              id="border-list">我是订单栏</el-col>
-      <el-col :span="7">我是产品栏</el-col>
+              id="border-list">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table :data="tableData"
+                      border
+                      style="width: 100%">
+              <el-table-column prop="goodsName"
+                               label="商品名称">
+              </el-table-column>1
+              <el-table-column prop="count"
+                               label="数量"
+                               width="50">
+              </el-table-column>
+              <el-table-column prop="price"
+                               label="金额"
+                               width="70">
+              </el-table-column>
+              <el-table-column label="操作"
+                               fixed="right"
+                               width="90">
+                <template>
+                  <el-button type="text"
+                             size="small">删除</el-button>
+                  <el-button type="text"
+                             size="small">增加</el-button>
+                </template>
+              </el-table-column>
+
+            </el-table>
+
+            <div class="div-btn">
+              <el-button type="warning">挂单</el-button>
+              <el-button type="danger">删除</el-button>
+              <el-button type="success">结账</el-button>
+
+            </div>
+
+          </el-tab-pane>
+          <el-tab-pane label="挂单">挂单</el-tab-pane>
+          <el-tab-pane label="外卖">外卖</el-tab-pane>
+
+        </el-tabs>
+      </el-col>
+      <el-col :span="14">
+        <div class="often-goods">
+          <div class="title">常用商品</div>
+          <div class="often-goods-list">
+            <ul>
+              <li v-for="({goodsName,price}) in oftenGoods">
+                <span>{{goodsName}}</span>
+                <span class="o-price">￥{{price}}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="goods-type">
+          <el-tabs>
+            <el-tab-pane label="汉堡">
+
+              <div>
+                <ul>
+                  <li v-for="({goodsImg,goodsName,price}) in type0Goods">
+                    <span class="foodImg"><img :src="goodsImg"></span>
+                    <span class="foodName">{{goodsName}} </span>
+                    <span class="foodPrice">{{price}} </span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="小食"></el-tab-pane>
+            <el-tab-pane label="饮料"></el-tab-pane>
+            <el-tab-pane label="套餐"></el-tab-pane>
+          </el-tabs>
+        </div>
+
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -12,6 +87,122 @@
 <script>
 export default {
   name: "posRmb",
+  data () {
+    return {
+      tableData: [{
+        goodsName: '可口可乐',
+        price: 8,
+        count: 1
+      }, {
+        goodsName: '香辣鸡腿堡',
+        price: 15,
+        count: 1
+      }, {
+        goodsName: '爱心薯条',
+        price: 8,
+        count: 1
+      }, {
+        goodsName: '甜筒',
+        price: 8,
+        count: 1
+      }],
+      oftenGoods: [{
+        goodsId: 1,
+        goodsName: '香辣鸡腿堡',
+        price: 18
+      }, {
+        goodsId: 2,
+        goodsName: '田园鸡腿堡',
+        price: 15
+      }, {
+        goodsId: 3,
+        goodsName: '和风汉堡',
+        price: 15
+      }, {
+        goodsId: 4,
+        goodsName: '快乐全家桶',
+        price: 80
+      }, {
+        goodsId: 5,
+        goodsName: '脆皮炸鸡腿',
+        price: 10
+      }, {
+        goodsId: 6,
+        goodsName: '魔法鸡块',
+        price: 20
+      }, {
+        goodsId: 7,
+        goodsName: '可乐大杯',
+        price: 10
+      }, {
+        goodsId: 8,
+        goodsName: '雪顶咖啡',
+        price: 18
+      }, {
+        goodsId: 9,
+        goodsName: '大块鸡米花',
+        price: 15
+      }, {
+        goodsId: 20,
+        goodsName: '香脆鸡柳',
+        price: 17
+      }],
+      type0Goods: [
+        {
+          goodsId: 1,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+          goodsName: '香辣鸡腿堡',
+          price: 18
+        }, {
+          goodsId: 2,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+          goodsName: '田园鸡腿堡',
+          price: 15
+        }, {
+          goodsId: 3,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+          goodsName: '和风汉堡',
+          price: 15
+        }, {
+          goodsId: 4,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+          goodsName: '快乐全家桶',
+          price: 80
+        }, {
+          goodsId: 5,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+          goodsName: '脆皮炸鸡腿',
+          price: 10
+        }, {
+          goodsId: 6,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
+          goodsName: '魔法鸡块',
+          price: 20
+        }, {
+          goodsId: 7,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
+          goodsName: '可乐大杯',
+          price: 10
+        }, {
+          goodsId: 8,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
+          goodsName: '雪顶咖啡',
+          price: 18
+        }, {
+          goodsId: 9,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+          goodsName: '大块鸡米花',
+          price: 15
+        }, {
+          goodsId: 20,
+          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
+          goodsName: '香脆鸡柳',
+          price: 17
+        }
+
+      ],
+    }
+  },
   mounted: function () {
     var orderHigh = document.body.clientHeight;
     console.log(orderHigh);
@@ -25,5 +216,58 @@ export default {
   background-color: #f9fafc;
   border-right: 1px solid #c0ccda;
   height: 100%;
+}
+.div-btn {
+  margin-top: 20px;
+  text-align: center;
+}
+.title {
+  height: 20px;
+  border-bottom: 1px solid #d3dce6;
+  background-color: #f9fafc;
+  padding: 10px;
+  text-align: center;
+}
+.often-goods-list ul li {
+  float: left;
+  border: 1px solid #f9fafc;
+  padding: 10px;
+  margin: 10px;
+  background-color: #f9fafc;
+}
+.o-price {
+  color: #58b7ff;
+}
+.goods-type {
+  clear: both;
+}
+/* 拷贝 */
+.cookList li {
+  list-style: none;
+  width: 23%;
+  border: 1px solid #e5e9f2;
+  height: auot;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 2px;
+  float: left;
+  margin: 2px;
+}
+.cookList li span {
+  display: block;
+  float: left;
+}
+.foodImg {
+  width: 40%;
+}
+.foodName {
+  font-size: 18px;
+  padding-left: 10px;
+  color: brown;
+}
+.foodPrice {
+  font-size: 16px;
+  padding-left: 10px;
+  padding-top: 10px;
 }
 </style>
--- src/views/posView.vue
+++ src/views/posView.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="pos">
-    <div class="main"></div>
-    <posRmb />
+    <div class="main">
+      <posRmb />
+    </div>
   </div>
 </template>
 
 <script>
 import posRmb from "@/components/page/posGp.vue";
+
 
 export default {
   components: { posRmb },
@@ -18,7 +20,7 @@ export default {
   float: left;
   width: 95%;
   height: 100%;
-  background-color: #20a6dc;
+  /* background-color: #20a6dc; */
   overflow: hidden;
 }
 </style>
